5c54e8327781cfe309de0ddd49c5d4ce3a087a6d
Dennis Meyer <den.meyer1@web.de>
2015-04-18 18:21:43 +0200

    viel spaß nutte

--- Assets/Scripts/Transformation.cs
+++ Assets/Scripts/Transformation.cs
@@ -7,10 +7,12 @@ public class Transformation : MonoBehaviour {
 	public Transform possword;
 	int roty = 0;
 
+	public Animator anim2;
+
 	//Vector3 rotplayer=new Vector3(0,0,0);
 	// Use this for initialization
 	void Start () {
-
+		anim2 = GetComponent<Animator>();
 	}
 
 	// Update is called once per frame
@@ -21,6 +23,7 @@ public class Transformation : MonoBehaviour {
 
 		transplayer = posplayer.transform.position;
 		transsword = possword.transform.position;
+
 
 
 		//rotplayer = posplayer.rotation.eulerAngles;
@@ -45,6 +48,51 @@ public class Transformation : MonoBehaviour {
 			transsword.y = transplayer.y;
 			roty = 0;
 		}
+
+		//Animations
+		if (Input.GetKeyDown (KeyCode.D & KeyCode.G)) {
+			transsword.x = transplayer.x + 0.137f;
+			transsword.y = transplayer.y;
+			roty = 180;
+			anim2.SetBool ("Attack", true);
+		} else {
+			anim2.SetBool("Attack",false);
+		}
+
+		if (Input.GetKeyDown (KeyCode.A & KeyCode.G)) {
+			transsword.x = transplayer.x - 0.116f;
+			transsword.y = transplayer.y;
+			roty = 0;
+			anim2.SetBool ("Attack", true);
+		} else {
+			anim2.SetBool("Attack",false);
+		}
+
+		if (Input.GetKeyDown (KeyCode.W & KeyCode.G)) {
+			transsword.x = transplayer.x + 0.115f;
+			transsword.y = transplayer.y;
+			roty = 180;
+			anim2.SetBool ("Attack", true);
+		} else {
+			anim2.SetBool("Attack",false);
+		}
+
+		if (Input.GetKeyDown (KeyCode.S & KeyCode.G)) {
+			transsword.x = transplayer.x - 0.129f;
+			transsword.y = transplayer.y;
+			roty = 0;
+			anim2.SetBool ("Attack", true);
+		} else {
+			anim2.SetBool("Attack",false);
+		}
+
+
+
+
+
+		
+
+
 		Debug.Log (transplayer + "_" );
 		Debug.Log (transsword + "_" + roty);
 		possword.transform.position=transsword;
